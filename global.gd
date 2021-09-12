0e9812bad2ba428c1d296f82fd9b85602012523a
Lerrrtaste <lerrrtaste@protonmail.com>
2021-09-12 19:32:50 +0200

    Moved some balancing variables to global a const

--- global.gd
+++ global.gd
@@ -4,3 +4,11 @@ extends Node
 const ORDER_PUKE_THRESHOLD = 5 #spawn puke after x seconds
 const ORDER_ZOMBIE_THRESHOLD_START = 15 #spawn weakest zombie after x seconds
 const ORDER_ZOMBIE_THRESHOLD_END = 25 #spawn strongest zombie after x seconds
+
+##enemies
+#puke
+const ENEMY_PUKE_SLOWDOWN_PERCENT = 0.5
+const ENEMY_PUKE_DAMAGE_IMPACT = 1
+const ENEMY_PUKE_LIFETIME = 15
+
+#zombies
